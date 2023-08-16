11fe029badc0f660b1a36c091b83308c29fd22d3
Your Name <lisleclara1@gmail.com>
2023-08-15 23:12:06 -0300

    GetStaticProps

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -35,14 +35,27 @@ export const getStaticPaths: GetStaticPaths = async ({locales}) => {
 }
 
 export const getStaticProps : GetStaticProps = async ({params}) => {
-  const id = params?.id as string
-  const character = await getCharacter(id)
-     
-  return {
-    props:{
-      character:character
-    }
-  }
+  const id = params?.id 
+  try {
+		const res = await fetch(
+			`https://www.amiiboapi.com/api/amiibo/?tail=${id}`
+		);
+		const data = await res.json();
+		const character = data.amiibo[0];
+		return {
+			props: {
+				character,
+			},
+		};
+	} catch (error) {
+		console.error('No se pudo obtener el personaje', error);
+		return {
+			props: {
+				character: {},
+			}
+		}
+	}
+	
 }
 
 export default CharacterPage
